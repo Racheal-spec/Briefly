4577f82d30f574137ef50000af454f4934e40238
PC <pc@PCs-MacBook-Pro.lan>
2025-03-24 14:49:49 +0100

    redesigned text box and body styles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from "react";
 import linkimg from "../assets/link.svg";
 import { useLazyGetSummaryQuery } from "../services/article";
+import copy from "../assets/copy.svg";
+import loader from "../assets/loader.svg";
+import tick from "../assets/tick.svg";
 
 const Demo = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
+  const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const [copied, setCopied] = "";
+
+  useEffect(() => {
+    const localstorageArticle = JSON.parse(localStorage.getItem("articles"));
+
+    if (localstorageArticle) {
+      setAllArticles(localstorageArticle);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({ articleUrl: article?.url });
@@ -12,14 +26,35 @@ const Demo = () => {
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
 
+      const updatedallArticles = [newArticle, ...allArticles];
+
       setArticle(newArticle);
+      setAllArticles(updatedallArticles);
 
       console.log(newArticle);
+      localStorage.setItem("articles", JSON.stringify(updatedallArticles));
     }
   };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
   return (
-    <section className="mt-16 w-full max-w-xl">
-      <div className="flex flex-col w-full gap-2">
+    <section className="mt-16 w-full flex items-center justify-center flex-col">
+      <h1 className="mt-10 text-5xl font-extrabold text-black sm:text-6xl text-center sm:text-6xl">
+        Summarize Any Article <br className="max-md:hidden" />
+        <span className="bg-gradient-to-r from-teal-500 via-green-600 to-lime-500 bg-clip-text text-transparent">
+          in Seconds
+        </span>
+      </h1>
+      <p className="mt-10 text-bas font-medium text-gray-600 text-center flex items-center justify-center sm:text-xl max-w-3xl">
+        Tired of reading long articles? Drop the link or text and get a clear,
+        concise summary. It's perfect for students, researchers, and busy
+        professionals.
+      </p>
+      <div className="flex flex-col w-full max-w-2xl pt-10 gap-2">
         <form
           className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
@@ -49,6 +84,55 @@ const Demo = () => {
             ↲
           </button>
         </form>
+        {/******Browse URL History*******/}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="p-3 flex justify-start items-center flex-row bg-white border border-gray-200 gap-3 rounded-lg cursor-pointer"
+            >
+              <div
+                onClick={() => handleCopy(item.url)}
+                className="w-7 h-7 rounded-full bg-white/10 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/******Display Results*******/}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <>
+            <p className="font-inter font-bold text-black text-center">
+              You have an error
+            </p>
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data.error}
+            </span>
+          </>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="text-3xl">Article Summary</h2>
+              <div className="p-10 mx-30 my-5 text-sm font-normal rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur p-4">
+                <p>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,17 @@
-import logo from "../assets/logo.svg";
+import newlogo from "../assets/newlogo.png";
 
 const Hero = () => {
   return (
-    <header>
-      <nav className="flex justify-between">
-        <img src={logo} alt="logo" />
+    <header className="w-full">
+      <nav className="flex justify-between px-10">
+        <img src={newlogo} alt="logo" className="w-40" />
         <button
-          className="bg-black text-white px-5 py-3 rounded-3xl"
+          className="bg-green-700 text-white px-5 rounded-2xl text-lg font-medium"
           onClick={() => window.open("https://github.com/Racheal-spec")}
         >
           Github
         </button>
       </nav>
-
-      <h1 className="mt-5 text-5xl font-extrabold text-black sm:text-6xl text-center sm:text-6xl">
-        Summarize Articles with <br className="max-md:hidden" />
-        <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
-          OpenAi GPT-4
-        </span>
-      </h1>
-      <p className="mt-5 text-lg text-gray-600 text-center sm:text-xl text-center max-w-2xl">
-        Simplify your reading with Summize, an open source article summarizer
-        that transforms lengthy articles into clear aconcise summaries.
-      </p>
     </header>
   );
 };
